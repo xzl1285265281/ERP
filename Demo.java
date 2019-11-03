2c7dcbc25fa60118e9177d818ae6afe4c02889a7
xzl <18256025235@163.com>
2019-11-03 10:47:57 +0800

    the commit message

--- Demo.java
+++ Demo.java
@@ -1,7 +1,7 @@
 public class hello{
 	public static void main(String[] args) {
 		System.out.println("hello git!!!");
-		System.out.println("哈哈哈哈");
+		System.out.println("25252558");
 		System.out.println("1111");
 		System.out.println("22222");
 	}
