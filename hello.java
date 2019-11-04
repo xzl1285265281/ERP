dae5aaea516571e6635b2e6a093df3c6e955afa5
xzl <18256025235@163.com>
2019-11-04 14:02:58 +0800

    In hello.java File Add tow Statement

--- hello.java
+++ hello.java
@@ -1,5 +1,8 @@
 public class hello{
 	public static void main(String[] args) {
 		System.out.println("hello git!");
+		System.out.println("给体贴!");
+		System.out.println("给他!");
+		System.out.println("给同!");
 	}
 }
