90490a171219a61f2ea8cd3a286837864f962de2
xzl <18256025235@163.com>
2019-11-16 08:30:31 +0800

    Change hello.java In dev branch

--- hello.java
+++ hello.java
@@ -1,10 +1,7 @@
+import java.util.*;
 public class hello{
 	public static void main(String[] args) {
 		System.out.println("hello git!");
-		System.out.println("给体贴!");
-		System.out.println("给他!");
-		System.out.println("给同1234!");
-		System.out.println("125252!");
-		System.out.println("128");
-	}		
+		Date date =new Date();
+	}
 }
