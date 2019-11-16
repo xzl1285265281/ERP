7e0077ee3b2eacc35f88b2c1f4b7725e24178ba0
xzl <18256025235@163.com>
2019-11-16 08:47:37 +0800

    Test

--- hello.java
+++ hello.java
@@ -6,9 +6,5 @@ public class hello{
 		System.out.println("给他!");
 		System.out.println("给同1234!");
 		System.out.println("125252!");
-		System.out.println("128");
-		Admin admin=new Admin();
-		Emp emp=new Emp();
-	}		
-		Date date =new Date();
+	}
 }
