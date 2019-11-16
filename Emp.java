667278055c9ad9386ff74e82d9e8a78dff89066d
xzl <18256025235@163.com>
2019-11-16 15:59:37 +0800

    Change Admin.java And Emp.java

--- Emp.java
+++ Emp.java
@@ -1,5 +1,9 @@
 public class Emp implements java.io.Serializable{
 	private Integer empno;
 	private String ename;
-	System.out.println("125");
+	System.out.println(125);
+	public String toString(){
+		return "lala ";
+	}
+
 }
